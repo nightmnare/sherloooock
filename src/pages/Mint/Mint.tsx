c89f258c091810d55eab4c7f23ca73047fa9bc14
YellowBearPooh <yellowbearpooh0110@gmail.com>
2022-06-27 11:55:23 +0900

    image box style update

--- src/pages/Mint/Mint.tsx
+++ src/pages/Mint/Mint.tsx
@@ -1,34 +1,17 @@
 import * as React from "react"
-import { Box } from "../../components/Box"
 import { Button } from "../../components/Button/Button"
+import { Column } from "../../components/Layout"
+import NFTImg from "../../assets/images/nfts/key.gif"
 
 import styles from "./Mint.module.scss"
 
-const imageSrcs: Array<string> = []
-const imageNumbers = Array.from({ length: 14 }, (_, i) => i + 1)
-;(async () => {
-  for (const number of imageNumbers) {
-    const { default: path } = await import(`../../assets/images/nfts/${number}.png`)
-    imageSrcs.push(path)
-  }
-})()
-
 export const MintPage: React.FC = () => {
-  const [activeImageIndex, setActiveImageIndex] = React.useState<number>(0)
-  React.useEffect(() => {
-    const timerRef = setInterval(() => {
-      setActiveImageIndex((_prev) => {
-        return _prev === imageNumbers.length - 1 ? 0 : _prev + 1
-      })
-    }, 500)
-    return () => {
-      clearInterval(timerRef)
-    }
-  }, [])
   return (
-    <Box>
-      <img className={styles.image} src={imageSrcs[activeImageIndex]} width={200} height={200} alt="nfts" />
+    <Column>
+      <div className={styles.imageWrapper}>
+        <img className={styles.image} src={NFTImg} alt="NFTImg" />
+      </div>
       <Button>Mint</Button>
-    </Box>
+    </Column>
   )
 }
